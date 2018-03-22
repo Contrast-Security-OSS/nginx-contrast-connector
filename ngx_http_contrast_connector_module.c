7328d5ca3d6d0f6973c4b1b4af40939cf727cc3d
Galen Palmer <galen.palmer@contrastsecurity.com>
2018-03-22 18:07:15 -0400

    saving the response filter code by moving into own c file

--- ngx_http_contrast_connector_module.c
+++ ngx_http_contrast_connector_module.c
@@ -26,12 +26,23 @@
 #include "ngx_http_contrast_connector_common.h"
 #include "ngx_http_contrast_connector_socket.h"
 
-static void * ngx_http_contrast_connector_create_loc_config(ngx_conf_t * cf);
 
 /* TODO: not using filters anymore...
 static ngx_int_t ngx_http_contrast_connector_module_header_filter(ngx_http_request_t * r);
 static ngx_int_t ngx_http_contrast_connector_module_body_filter(ngx_http_request_t * r, ngx_chain_t * chain);
 */
+
+/*
+ * allocate and initialize location configuration
+ */
+static void * ngx_http_contrast_connector_create_loc_config(ngx_conf_t * cf);
+
+/*
+ * merge location configurations
+ */
+static char * ngx_http_contrast_connector_merge_loc_config(ngx_conf_t * cf, 
+		void * parent, 
+		void * child);
 
 static ngx_int_t ngx_http_contrast_connector_module_init(ngx_conf_t * cf);
 static char * ngx_http_contrast_connector(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
@@ -44,14 +55,9 @@ static u_char * append_request_body(u_char * dest, size_t dest_len, u_char * src
 */
 
 /*
- * static reference to next header filter callback
- */
-static ngx_http_output_header_filter_pt ngx_http_next_header_filter;
-
-/*
  * static reference to next body filter callback
  */
-static ngx_http_output_body_filter_pt ngx_http_next_body_filter;
+static ngx_http_request_body_filter_pt ngx_http_next_request_body_filter;
 
 /*
  * get the current epoch time in millis
@@ -119,40 +125,6 @@ static ngx_command_t ngx_http_contrast_connector_commands[] = {
         ngx_null_command
 };
 
-/*
- * create location configuration
- */
-static void * ngx_http_contrast_connector_create_loc_config(ngx_conf_t * cf)
-{
-    ngx_http_contrast_connector_conf_t * conf;
-    conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_contrast_connector_conf_t));
-
-    if (conf == NULL) {
-        return NGX_CONF_ERROR;
-    }
-
-    conf->enable = NGX_CONF_UNSET;
-    conf->debug = NGX_CONF_UNSET;
-    return conf;
-}
-
-/*
- * merge location configurations
- */
-static char * ngx_http_contrast_connector_merge_loc_config(ngx_conf_t * cf, void * parent, void * child)
-{
-    ngx_http_contrast_connector_conf_t * prev = parent;
-    ngx_http_contrast_connector_conf_t * conf = child;
-
-    ngx_conf_merge_off_value(conf->enable, prev->enable, NGX_CONF_UNSET);
-    ngx_conf_merge_off_value(conf->debug, prev->debug, NGX_CONF_UNSET);
-    ngx_conf_merge_str_value(conf->socket_path, prev->socket_path, "/tmp/contrast-security.sock");
-
-    if (conf->debug > 0) {
-		dd("config enabled socket_path=%s", conf->socket_path.data);
-    }
-    return NGX_CONF_OK;
-}
 
 /*
  * module context structure
@@ -226,217 +198,46 @@ ngx_module_t ngx_http_contrast_connector_module = {
 };
 
 /*
- * act as filter for headers in request
+ * default socket to communicate with contrast service
  */
-/* TODO not using filters anymore 
-static ngx_int_t ngx_http_contrast_connector_module_header_filter(ngx_http_request_t * r)
-{
-    fprintf(stderr, "ENTER: ngx_http_contrast_connector_module_header_filter\n");
-    return ngx_http_next_header_filter(r);
-}
-*/
+static const char DEFAULT_SOCKET[] = "/tmp/contrast-security.sock";
 
 /*
- * act as filter for request body
+ * create location configuration
  */
-/* TODO: not using filters anymore...
-static ngx_int_t ngx_http_contrast_connector_module_body_filter(ngx_http_request_t * r, ngx_chain_t * in)
+static void * ngx_http_contrast_connector_create_loc_config(ngx_conf_t * cf)
 {
-    fprintf(stderr, "ENTER: ngx_http_contrast_connector_module_body_filter\n");
+    ngx_http_contrast_connector_conf_t * conf;
+    conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_contrast_connector_conf_t));
 
-	int buffer_fully_loaded = 0;
-	ngx_chain_t *chain = NULL;
-	
-    ngx_http_contrast_connector_conf_t * conf = NULL;
-	ngx_http_headers_in_t hin = r->headers_in;
-	ngx_list_t headers = hin.headers;
-	ngx_list_part_t * curr = NULL;
-	ngx_table_elt_t * entry_ptr = NULL;
-	ngx_table_elt_t * entry = NULL;
-	int count = 0;
-	u_char * dest = NULL;
-	size_t dest_len = 0;
-
-	if (in == NULL) {
-		fprintf(stderr, "WARN: passing to next filter early since the chain was NULL\n");
-		return ngx_http_next_body_filter(r, in);
-	}
-
-	for (chain = in; chain != NULL; chain = chain->next) {
-		if (chain->buf->last_buf) { buffer_fully_loaded = 1; }
-	}
-	if (buffer_fully_loaded == 0) {
-		fprintf(stderr, "WARN: buffer not fully loaded so passing to filter chain\n");
-		u_char * tmp = read_request_body(r, in);
-		fprintf(stderr, "INFO: the current contents are %s\n\n", tmp);
-		if (tmp != NULL) {
-			free(tmp);
-		}
-
-		return ngx_http_next_body_filter(r, in);
-	}
-	
-    conf = ngx_http_get_module_loc_conf(r, ngx_http_contrast_connector_module);
     if (conf == NULL) {
-        fprintf(stderr, "WARN: local configuration was NULL\n");
-		return ngx_http_next_body_filter(r, in);
+        return NGX_CONF_ERROR;
     }
+
+    conf->enable = NGX_CONF_UNSET;
+    conf->debug = NGX_CONF_UNSET;
+    return conf;
+}
+
+/*
+ * merge location configurations
+ */
+static char * ngx_http_contrast_connector_merge_loc_config(ngx_conf_t * cf, 
+		void * parent, 
+		void * child)
+{
+    ngx_http_contrast_connector_conf_t * prev = parent;
+    ngx_http_contrast_connector_conf_t * conf = child;
+
+    ngx_conf_merge_off_value(conf->enable, prev->enable, NGX_CONF_UNSET);
+    ngx_conf_merge_off_value(conf->debug, prev->debug, NGX_CONF_UNSET);
+    ngx_conf_merge_str_value(conf->socket_path, prev->socket_path, DEFAULT_SOCKET);
 
     if (conf->debug > 0) {
-        fprintf(stderr, "DEBUG: in body filter\n");
-
-		fprintf(stderr, "INFO: uri = %s (%ld)\n", r->uri.data, r->uri.len);
-		fprintf(stderr, "INFO: unparsed uri = %s (%ld)\n", r->unparsed_uri.data, r->unparsed_uri.len);
-		fprintf(stderr, "INFO: args = %s (%ld)\n", r->args.data, r->args.len);
-		fprintf(stderr, "INFO: request iine = %s (%ld)\n", r->request_line.data, r->request_line.len);
-		fprintf(stderr, "INFO: method_name = %s (%ld)\n", r->method_name.data, r->method_name.len);
-		fprintf(stderr, "INFO: http protocol = %s (%ld)\n", r->http_protocol.data, r->http_protocol.len);
-		fprintf(stderr, "INFO: addr = %s (%ld)\n", r->connection->addr_text.data, r->connection->addr_text.len);
-		fprintf(stderr, "INFO: proxy protocol addr = %s (%ld)\n", r->connection->proxy_protocol_addr.data, r->connection->proxy_protocol_addr.len);
-		fprintf(stderr, "INFO: proxy protocol port = %d\n", r->connection->proxy_protocol_port);
-
-		if (r->parent != NULL) {
-			fprintf(stderr, "WARN: parent structure was not NULL\n");
-		}
-
-
-		fprintf(stderr, "DEBUG: about to call nalloc on headers list\n");
-		if (headers.nalloc > 0) {
-			fprintf(stderr, "DEBUG: the value from nalloc was %ld: size=%ld\n", headers.nalloc, headers.size);
-			curr = &headers.part;
-			entry_ptr = (ngx_table_elt_t *)curr->elts;
-
-			fprintf(stderr, "DEBUG: current nelts is %ld\n", curr->nelts);
-			for(count = 0; ; count++) {
-				if (count >= curr->nelts) {
-					if (curr->next == NULL) {
-						break;
-					}
-
-					curr = curr->next;
-					entry_ptr = (ngx_table_elt_t *)curr->elts;
-				}
-
-				entry = (&entry_ptr[count]);
-				fprintf(stderr, "INFO: count: %d key: %s value: %s\n", count, entry->key.data, entry->value.data);
-			}
-		} else {
-			fprintf(stderr, "WARN: headers allocated was 0\n");
-		}
+		dd("config enabled socket_path=%s", conf->socket_path.data);
     }
-
-    if (conf->enable > 0) {
-        fprintf(stderr, "DEBUG: body filter enabled\n");
-		Contrast__Api__Dtm__Message message = CONTRAST__API__DTM__MESSAGE__INIT;
-		Contrast__Api__Dtm__RawRequest dtm = CONTRAST__API__DTM__RAW_REQUEST__INIT;
-		Contrast__Api__Dtm__SimplePair pair = CONTRAST__API__DTM__SIMPLE_PAIR__INIT;
-
-		fprintf(stderr, "DEBUG: getting timestamp\n");
-		struct timeval tv;
-		gettimeofday(&tv, NULL);
-		dtm.timestamp_ms = (tv.tv_sec * 1000 + tv.tv_usec / 1000);
-
-		fprintf(stderr, "DEBUG: getting request line and normalized uri\n");
-		dtm.request_line = r->request_line.data;
-		dtm.normalized_uri = r->uri.data;
-
-		fprintf(stderr, "DEBUG: getting ip address\n");
-		struct sockaddr_in *sin;
-		ngx_addr_t addr;
-		char ipv4[INET_ADDRSTRLEN];
-
-		addr.sockaddr = r->connection->sockaddr;
-		addr.socklen = r->connection->socklen;
-		if (addr.sockaddr->sa_family == AF_INET) {
-			sin = (struct sockaddr_in *)addr.sockaddr;
-			if (sin != NULL) {
-				dtm.port = sin->sin_port;
-
-				inet_ntop(AF_INET, &(sin->sin_addr), ipv4, INET_ADDRSTRLEN);
-				dtm.ip = ipv4;
-				dtm.ip_version = 4;
-				fprintf(stderr, "INFO: getting address from socket %s:%d\n", dtm.ip, dtm.port);
-			}
-		}
-
-		if (headers.nalloc > 0) {
-			fprintf(stderr, "DEBUG: getting headers\n");
-			curr = &headers.part;
-			entry_ptr = (ngx_table_elt_t *)curr->elts;
-			for(count = 0; ; count++) {
-				if (count >= curr->nelts) {
-					if (curr->next == NULL) {
-						break;
-					}
-
-					curr = curr->next;
-					entry_ptr = (ngx_table_elt_t *)curr->elts;
-					count = 0;
-				}
-
-				entry = (&entry_ptr[count]);
-				pair.key = entry->key.data;
-				pair.value = entry->value.data;
-				fprintf(stderr, "INFO: count=%d key=%s value=%s\n", count, pair.key, pair.value);
-			}
-		}
-
-		fprintf(stderr, "DEBUG: getting request body\n");
-		dtm.request_body = read_request_body(r, in);
-		if (dtm.request_body != NULL) {
-			fprintf(stderr, "DEBUG: about to free dest\n");
-			free(dtm.request_body);
-		}
-    }
-
-
-    return ngx_http_next_body_filter(r, in);
+    return NGX_CONF_OK;
 }
-*/
-
-/* TODO: this is the response body... misnames
-static u_char * read_request_body(ngx_http_request_t * r, ngx_chain_t * in) 
-{
-	if (in == NULL) {
-		return NULL;
-	}
-
-	u_char * dest = NULL;
-	size_t dest_len = 0;
-
-	ngx_chain_t * chain = NULL;
-	u_char * src = NULL;
-	size_t src_len = 0;
-
-	for (chain = in; chain != NULL; chain = chain->next) {
-		src = chain->buf->start;
-		src_len = chain->buf->end - src;
-		fprintf(stderr, "DEBUG: buffer length = %ld\n", src_len);
-
-		dest = append_request_body(dest, dest_len, src, src_len, r->connection->log);
-		dest_len += src_len;
-		fprintf(stderr, "DEBUG: after append %s (%ld)\n", dest, dest_len);
-	}
-
-	return dest;
-}
-
-static u_char * append_request_body(u_char * dest, size_t dest_len, u_char * src, size_t src_len, ngx_log_t *log)
-{
-	size_t new_len = src_len + dest_len;
-	u_char * tmp = NULL;
-
-	if (dest == NULL) {
-		dest = ngx_calloc(src_len + 1, log);
-		strncpy(dest, src, src_len);
-	} else {
-		tmp = realloc(dest, new_len + 1);
-		strncpy(tmp + dest_len, src, src_len);
-		dest = tmp;
-	}
-	return dest;
-}
-*/
 
 /*
  * init module and place in the filter chain
---
+++ ngx_http_contrast_connector_response_filters.c
@@ -0,0 +1,188 @@
+#include "dtm.pb-c.h"
+#include "settings.pb-c.h"
+#include "ngx_http_contrast_connector_common.h"
+
+
+/*
+ * static reference to next header filter callback
+ */
+static ngx_http_output_header_filter_pt ngx_http_next_header_filter;
+
+/*
+ * static reference to next body filter callback
+ */
+static ngx_http_output_body_filter_pt ngx_http_next_body_filter;
+
+/*
+ * given buffer attempt to append from src; return ptr to new buffer location
+ */
+static u_char * append_request_body(u_char * dest, 
+		size_t dest_len, 
+		u_char * src, 
+		size_t src_len, 
+		ngx_log_t *log)
+{
+	size_t new_len = src_len + dest_len;
+	u_char * tmp = NULL;
+
+	if (dest == NULL) {
+		dest = ngx_calloc(src_len + 1, log);
+		strncpy(dest, src, src_len);
+	} else {
+		tmp = realloc(dest, new_len + 1);
+		strncpy(tmp + dest_len, src, src_len);
+		dest = tmp;
+	}
+	return dest;
+}
+
+/*
+ * read the entire chain 
+ */
+static u_char * read_request_body(ngx_http_request_t * r, ngx_chain_t * in) 
+{
+	if (in == NULL) {
+		return NULL;
+	}
+
+	u_char * dest = NULL;
+	size_t dest_len = 0;
+
+	ngx_chain_t * chain = NULL;
+	u_char * src = NULL;
+	size_t src_len = 0;
+
+	for (chain = in; chain != NULL; chain = chain->next) {
+		src = chain->buf->start;
+		src_len = chain->buf->end - src;
+		dd("buffer length = %ld", src_len);
+
+		dest = append_request_body(dest, dest_len, src, src_len, r->connection->log);
+		dest_len += src_len;
+		dd("after append len=%ld: %s", dest_len, dest);
+	}
+
+	return dest;
+}
+
+
+/*
+ * act as filter for headers in request
+ */
+static ngx_int_t ngx_http_contrast_connector_module_header_filter(ngx_http_request_t * r)
+{
+	dd("in response header fitler");
+    return ngx_http_next_header_filter(r);
+}
+
+/*
+ * act as filter for request body
+ */
+static ngx_int_t ngx_http_contrast_connector_module_body_filter(ngx_http_request_t * r, 
+		ngx_chain_t * in)
+{
+	dd("in response body filter");
+
+	int buffer_fully_loaded = 0;
+	ngx_chain_t *chain = NULL;
+	
+    ngx_http_contrast_connector_conf_t * conf = NULL;
+	ngx_http_headers_in_t hin = r->headers_in;
+	ngx_list_t headers = hin.headers;
+	ngx_list_part_t * curr = NULL;
+	ngx_table_elt_t * entry_ptr = NULL;
+	ngx_table_elt_t * entry = NULL;
+	int count = 0;
+	u_char * dest = NULL;
+	size_t dest_len = 0;
+
+	if (in == NULL) {
+		dd("[WARN] chain was NULL");
+		return ngx_http_next_body_filter(r, in);
+	}
+
+	for (chain = in; chain != NULL; chain = chain->next) {
+		if (chain->buf->last_buf) { buffer_fully_loaded = 1; }
+	}
+	if (buffer_fully_loaded == 0) {
+		dd("[WARN] buffer not fully loaded");	
+
+		u_char * tmp = read_request_body(r, in);
+		if (tmp != NULL) {
+			dd("the current contents are: %s", tmp);
+			free(tmp);
+		}
+
+		return ngx_http_next_body_filter(r, in);
+	}
+	
+    conf = ngx_http_get_module_loc_conf(r, ngx_http_contrast_connector_module);
+    if (conf == NULL) {
+		dd("[WARN] current config was NULL");
+		return ngx_http_next_body_filter(r, in);
+    }
+
+    if (conf->enable > 0) {
+		Contrast__Api__Dtm__Message message = CONTRAST__API__DTM__MESSAGE__INIT;
+		Contrast__Api__Dtm__RawRequest dtm = CONTRAST__API__DTM__RAW_REQUEST__INIT;
+		Contrast__Api__Dtm__SimplePair pair = CONTRAST__API__DTM__SIMPLE_PAIR__INIT;
+
+		struct timeval tv;
+		gettimeofday(&tv, NULL);
+		dtm.timestamp_ms = (tv.tv_sec * 1000 + tv.tv_usec / 1000);
+		dd("timestap=%ld", dtm.timestamp_ms);
+
+		dtm.request_line = r->request_line.data;
+		dtm.normalized_uri = r->uri.data;
+		dd("request_line=%s normalized_uri=%s", dtm.request_line, dtm.normalized_uri);
+
+		struct sockaddr_in *sin;
+		ngx_addr_t addr;
+		char ipv4[INET_ADDRSTRLEN];
+
+		addr.sockaddr = r->connection->sockaddr;
+		addr.socklen = r->connection->socklen;
+		if (addr.sockaddr->sa_family == AF_INET) {
+			sin = (struct sockaddr_in *)addr.sockaddr;
+			if (sin != NULL) {
+				dtm.port = sin->sin_port;
+
+				inet_ntop(AF_INET, &(sin->sin_addr), ipv4, INET_ADDRSTRLEN);
+				dtm.ip = ipv4;
+				dtm.ip_version = 4;
+				dd("address from socket: %s:%d", dtm.ip, dtm.port);
+			}
+		}
+
+		if (headers.nalloc > 0) {
+			curr = &headers.part;
+			entry_ptr = (ngx_table_elt_t *)curr->elts;
+			for(count = 0; ; count++) {
+				if (count >= curr->nelts) {
+					if (curr->next == NULL) {
+						break;
+					}
+
+					curr = curr->next;
+					entry_ptr = (ngx_table_elt_t *)curr->elts;
+					count = 0;
+				}
+
+				entry = (&entry_ptr[count]);
+				pair.key = entry->key.data;
+				pair.value = entry->value.data;
+				dd("header: %s=%s (%d)", pair.key, pair.value, count);
+			}
+		}
+
+		dtm.request_body = read_request_body(r, in);
+		if (dtm.request_body != NULL) {
+			dd("complete request body: %s", dtm.request_body);
+			free(dtm.request_body);
+		}
+    }
+
+    return ngx_http_next_body_filter(r, in);
+}
+
+
