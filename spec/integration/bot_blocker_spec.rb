db8c44a6408ad36622221e8ce0be88b5dca435ef
Galen Palmer <galen.palmer@contrastsecurity.com>
2018-04-11 09:38:51 -0400

    CONTRAST-22213 unsigned chars in message length conversion; test for virtual patch

--- spec/integration/bot_blocker_spec.rb
+++ spec/integration/bot_blocker_spec.rb
@@ -11,24 +11,24 @@ describe "Integration Specs" do
     `build/nginx/sbin/nginx -s stop`
   end
   
-    describe "BotBlocker" do
-      it "allows a request without a user agent" do
-        http = Curl.get("http://127.0.0.1:8888") do |curl|
-          curl.headers['User-Agent'] = ""
-        end
-        expect(http.response_code).to eq(200)
+  describe "BotBlocker" do
+    it "allows a request without a user agent" do
+      http = Curl.get("http://127.0.0.1:8888") do |curl|
+        curl.headers['User-Agent'] = ""
       end
-      it "allows a request with a user agent" do
-        http = Curl.get("http://127.0.0.1:8888") do |curl|
-          curl.headers['User-Agent'] = "Mozilla"
-        end
-        expect(http.response_code).to eq(200)
-      end
-      it "blocks a request with a bad user agent" do
-        http = Curl.get("http://127.0.0.1:8888") do |curl|
-          curl.headers['User-Agent'] = "Go!Zilla"
-        end
-        expect(http.response_code).to eq(403)
-      end
+      expect(http.response_code).to eq(200)
     end
+    it "allows a request with a user agent" do
+      http = Curl.get("http://127.0.0.1:8888") do |curl|
+        curl.headers['User-Agent'] = "Mozilla"
+      end
+      expect(http.response_code).to eq(200)
+    end
+    it "blocks a request with a bad user agent" do
+      http = Curl.get("http://127.0.0.1:8888") do |curl|
+        curl.headers['User-Agent'] = "Go!Zilla"
+      end
+      expect(http.response_code).to eq(403)
+    end
+  end
 end
