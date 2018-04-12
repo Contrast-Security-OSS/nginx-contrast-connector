b22a2de384b067bab17f25530b6b29b455396240
Galen Palmer <galen.palmer@contrastsecurity.com>
2018-04-12 13:25:21 -0400

    CONTRAST-22301 unreleated spec for IP in a CIDR block

--- spec/integration/ip_blacklist_spec.rb
+++ spec/integration/ip_blacklist_spec.rb
@@ -19,5 +19,14 @@ describe "Integration Specs" do
       end
       expect(http.response_code).to eq(403)
     end
+
+    it "blocked from 20.20.20.0/24 ip block (if setup in Teamserver)" do
+      http = Curl.get("http://127.0.0.1:8888") do |curl|
+        curl.headers['User-Agent'] = "Bob's House of Software"
+        curl.headers['X-Forwarded-For'] = "20.20.20.0"
+      end
+      expect(http.response_code).to eq(403)
+
+    end
   end
 end
