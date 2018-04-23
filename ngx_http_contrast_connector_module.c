5cc8edd9fafee69877917b5dfa9393d571b76e98
Galen Palmer <galen.palmer@contrastsecurity.com>
2018-04-23 09:55:10 -0400

    CONTRAST-22525 handler applies configuration name; concerned about allocation of app name

--- ngx_http_contrast_connector_module.c
+++ ngx_http_contrast_connector_module.c
@@ -61,53 +61,61 @@ ngx_inline int64_t unix_millis()
  */
 ngx_inline char * ngx_str_to_char(ngx_str_t a, ngx_pool_t * p)
 {
-    char * str = NULL;
-    if (a.len == 0) {
-        /* string is empty; return NULL */
-        return NULL;
-    }
+	char * str = NULL;
+	if (a.len == 0) {
+		/* string is empty; return NULL */
+		return NULL;
+	}
 
-    str = ngx_pnalloc(p, a.len + 1);
-    if (str == NULL) {
-        /* string could not be allocated; return -1 */
-        return (char *) -1;
-    }
+	str = ngx_pnalloc(p, a.len + 1);
+	if (str == NULL) {
+		/* string could not be allocated; return -1 */
+		return (char *) -1;
+	}
 
-    /* return 0 terminated string */
-    ngx_memcpy(str, a.data, a.len);
-    str[a.len] = '\0';
-    return str;
+	/* return 0 terminated string */
+	ngx_memcpy(str, a.data, a.len);
+	str[a.len] = '\0';
+	return str;
 }
 
 /*
  * available commands for module
  */
 static ngx_command_t ngx_http_contrast_connector_commands[] = {
-        {
-                ngx_string("contrast"),
-                NGX_HTTP_LOC_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_MAIN_CONF | NGX_CONF_FLAG,
-                ngx_conf_set_flag_slot,
-                NGX_HTTP_LOC_CONF_OFFSET,
-                offsetof(ngx_http_contrast_connector_conf_t, enable),
-                NULL
-        },
-        {
-                ngx_string("contrast_debug"),
-                NGX_HTTP_LOC_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_MAIN_CONF | NGX_CONF_FLAG,
-                ngx_conf_set_flag_slot,
-                NGX_HTTP_LOC_CONF_OFFSET,
-                offsetof(ngx_http_contrast_connector_conf_t, debug),
-                NULL
-        },
-        {
-                ngx_string("contrast_unix_socket"),
-                NGX_HTTP_LOC_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_MAIN_CONF | NGX_CONF_TAKE1,
-                ngx_conf_set_str_slot,
-                NGX_HTTP_LOC_CONF_OFFSET,
-                offsetof(ngx_http_contrast_connector_conf_t, socket_path),
-                NULL
-        },
-        ngx_null_command
+		{
+				ngx_string("contrast"),
+				NGX_HTTP_LOC_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_MAIN_CONF | NGX_CONF_FLAG,
+				ngx_conf_set_flag_slot,
+				NGX_HTTP_LOC_CONF_OFFSET,
+				offsetof(ngx_http_contrast_connector_conf_t, enable),
+				NULL
+		},
+		{
+				ngx_string("contrast_debug"),
+				NGX_HTTP_LOC_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_MAIN_CONF | NGX_CONF_FLAG,
+				ngx_conf_set_flag_slot,
+				NGX_HTTP_LOC_CONF_OFFSET,
+				offsetof(ngx_http_contrast_connector_conf_t, debug),
+				NULL
+		},
+		{
+				ngx_string("contrast_unix_socket"),
+				NGX_HTTP_LOC_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_MAIN_CONF | NGX_CONF_TAKE1,
+				ngx_conf_set_str_slot,
+				NGX_HTTP_LOC_CONF_OFFSET,
+				offsetof(ngx_http_contrast_connector_conf_t, socket_path),
+				NULL
+		},
+		{
+				ngx_string("contrast_app_name"),
+				NGX_HTTP_LOC_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_MAIN_CONF | NGX_CONF_TAKE1,
+				ngx_conf_set_str_slot,
+				NGX_HTTP_LOC_CONF_OFFSET,
+				offsetof(ngx_http_contrast_connector_conf_t, app_name),
+				NULL
+		},
+		ngx_null_command
 };
 
 
@@ -116,29 +124,29 @@ static ngx_command_t ngx_http_contrast_connector_commands[] = {
  */
 static ngx_http_module_t ngx_http_contrast_connector_module_ctx = {
 
-        /* pre configuration */
-        NULL,
+		/* pre configuration */
+		NULL,
 
-        /* post configuration */
-        ngx_http_contrast_connector_module_init,
+		/* post configuration */
+		ngx_http_contrast_connector_module_init,
 
-        /* create main configuration */
-        NULL,
+		/* create main configuration */
+		NULL,
 
-        /* init main configuration */
-        NULL,
+		/* init main configuration */
+		NULL,
 
-        /* create server configuration */
-        NULL,
+		/* create server configuration */
+		NULL,
 
-        /* merge server configuration */
-        NULL,
+		/* merge server configuration */
+		NULL,
 
-        /* create location configuration */
-        ngx_http_contrast_connector_create_loc_config,
+		/* create location configuration */
+		ngx_http_contrast_connector_create_loc_config,
 
-        /* merge location configuration */
-        ngx_http_contrast_connector_merge_loc_config
+		/* merge location configuration */
+		ngx_http_contrast_connector_merge_loc_config
 };
 
 /*
@@ -146,40 +154,40 @@ static ngx_http_module_t ngx_http_contrast_connector_module_ctx = {
  */
 ngx_module_t ngx_http_contrast_connector_module = {
 
-        NGX_MODULE_V1,
+		NGX_MODULE_V1,
 
-        /* address of module context */
-        &ngx_http_contrast_connector_module_ctx,
+		/* address of module context */
+		&ngx_http_contrast_connector_module_ctx,
 
-        /* module directives */
-        ngx_http_contrast_connector_commands,
+		/* module directives */
+		ngx_http_contrast_connector_commands,
 
-        /* module type */
-        NGX_HTTP_MODULE,
+		/* module type */
+		NGX_HTTP_MODULE,
 
-        /* init master */
-        NULL,
+		/* init master */
+		NULL,
 
-        /* init module */
-        NULL,
+		/* init module */
+		NULL,
 
-        /* init process */
-        NULL,
+		/* init process */
+		NULL,
 
-        /* init thread */
-        NULL,
+		/* init thread */
+		NULL,
 
-        /* exit thread */
-        NULL,
+		/* exit thread */
+		NULL,
 
-        /* exit process */
-        NULL,
+		/* exit process */
+		NULL,
 
-        /* exit master */
-        NULL,
+		/* exit master */
+		NULL,
 
 		/* required */
-        NGX_MODULE_V1_PADDING
+		NGX_MODULE_V1_PADDING
 };
 
 /*
@@ -188,20 +196,25 @@ ngx_module_t ngx_http_contrast_connector_module = {
 static const char DEFAULT_SOCKET[] = "/tmp/contrast-security.sock";
 
 /*
+ * default name for applications in contrast service
+ */
+static const char DEFAULT_NAME[] = "nginx";
+
+/*
  * create location configuration
  */
 static void * ngx_http_contrast_connector_create_loc_config(ngx_conf_t * cf)
 {
-    ngx_http_contrast_connector_conf_t * conf;
-    conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_contrast_connector_conf_t));
+	ngx_http_contrast_connector_conf_t * conf;
+	conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_contrast_connector_conf_t));
 
-    if (conf == NULL) {
-        return NGX_CONF_ERROR;
-    }
+	if (conf == NULL) {
+		return NGX_CONF_ERROR;
+	}
 
-    conf->enable = NGX_CONF_UNSET;
-    conf->debug = NGX_CONF_UNSET;
-    return conf;
+	conf->enable = NGX_CONF_UNSET;
+	conf->debug = NGX_CONF_UNSET;
+	return conf;
 }
 
 /*
@@ -211,17 +224,20 @@ static char * ngx_http_contrast_connector_merge_loc_config(ngx_conf_t * cf,
 		void * parent, 
 		void * child)
 {
-    ngx_http_contrast_connector_conf_t * prev = parent;
-    ngx_http_contrast_connector_conf_t * conf = child;
+	ngx_http_contrast_connector_conf_t * prev = parent;
+	ngx_http_contrast_connector_conf_t * conf = child;
 
-    ngx_conf_merge_off_value(conf->enable, prev->enable, NGX_CONF_UNSET);
-    ngx_conf_merge_off_value(conf->debug, prev->debug, NGX_CONF_UNSET);
-    ngx_conf_merge_str_value(conf->socket_path, prev->socket_path, DEFAULT_SOCKET);
+	ngx_conf_merge_off_value(conf->enable, prev->enable, NGX_CONF_UNSET);
+	ngx_conf_merge_off_value(conf->debug, prev->debug, NGX_CONF_UNSET);
+	ngx_conf_merge_str_value(conf->socket_path, prev->socket_path, DEFAULT_SOCKET);
 
-    if (conf->debug > 0) {
+	dd("APP NAME = %s %s", conf->app_name.data, prev->app_name.data); 
+	ngx_conf_merge_str_value(conf->app_name, prev->app_name, DEFAULT_NAME);
+
+	if (conf->debug > 0) {
 		dd("config enabled socket_path=%s", conf->socket_path.data);
-    }
-    return NGX_CONF_OK;
+	}
+	return NGX_CONF_OK;
 }
 
 /*
@@ -229,7 +245,8 @@ static char * ngx_http_contrast_connector_merge_loc_config(ngx_conf_t * cf,
  */
 static ngx_int_t ngx_http_contrast_connector_module_init(ngx_conf_t * cf)
 {
-	ngx_http_core_main_conf_t *main_conf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
+	ngx_http_core_main_conf_t *main_conf = ngx_http_conf_get_module_main_conf(cf, 
+		ngx_http_core_module);
 	if (main_conf == NULL) {
 		dd("Main conf was NULL");
 		return NGX_ERROR;
@@ -250,7 +267,7 @@ static ngx_int_t ngx_http_contrast_connector_module_init(ngx_conf_t * cf)
 	}
 
 	dd("Completed initialization of contrast connector module");
-    return NGX_OK;
+	return NGX_OK;
 }
 
 
