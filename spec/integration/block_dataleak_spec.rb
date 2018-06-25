b4d52ad270762cbcf6bd7a970ff0bb4e67b4068e
Galen Palmer <galen.palmer@contrastsecurity.com>
2018-06-25 13:56:44 -0400

    CONTRAST-24371 moving nginx start the beginning of the entire test suite

--- spec/integration/block_dataleak_spec.rb
+++ spec/integration/block_dataleak_spec.rb
@@ -2,15 +2,6 @@ require "spec_helper"
 
 describe "Integration Specs" do
 
-  before(:all) do
-    `build/nginx/sbin/nginx`
-    sleep 1
-  end
-
-  after(:all) do
-    `build/nginx/sbin/nginx -s stop`
-  end
-  
   describe "Data Leakage" do
     describe "GET" do
       it "allows a request to a file in a dir" do
--- spec/integration/virtual_patch_spec.rb
+++ spec/integration/virtual_patch_spec.rb
@@ -2,15 +2,6 @@ require 'spec_helper'
 
 describe "Integration Specs" do
   
-  before(:all) do
-    `build/nginx/sbin/nginx`
-    sleep 1
-  end
-
-  after(:all) do
-    `build/nginx/sbin/nginx -s stop`
-  end
-
   describe "VirtualPatch" do
     it "can block a request with a matching parameter value (if defined in Teamserver)" do
       http = Curl.get("http://127.0.0.1:8888", { virtual_patch: true }) do |curl|
