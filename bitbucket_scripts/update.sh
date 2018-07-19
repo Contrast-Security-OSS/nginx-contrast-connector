d355a78eb71817ee0cea8fd50f9d6f52f4c94e9e
Alex Boyd <alex.boyd@contrastsecurity.com>
2018-07-19 12:08:22 -0400

    remove caches to prevent stale git clones
    test clone
    
    add --no-cache to build option to prevent stale code
    
    tag without firsttry
    
    move docker build to pipelines instead of shell
    
    pull?
    
    no cache build
    
    pull while building
    
    check whats in container test
    
    remove ls from dockerfile and test pull in speedracer

--- bitbucket_scripts/update.sh
+++ bitbucket_scripts/update.sh
@@ -8,9 +8,10 @@ setup_directory() {
 
 build_base_image() {
     local imageId="123";
-    docker build -t universal-base-image . && \
+    docker build --no-cache -t universal-base-image . && \
     imageId=$(docker image ls -q | head -n 1) && \
-    docker tag $imageId contrastsecurity-docker-local.jfrog.io/universal-agent-base:firsttry
+    echo "About to upload the following image: ${imageId}" && \
+    docker tag $imageId contrastsecurity-docker-local.jfrog.io/universal-agent-base
 }
 
 push_image() {
